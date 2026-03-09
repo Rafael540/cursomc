b3a4ffa085738e7a78126c8472fdfa024c4416a8
Rafael Alves <alves540@hotmail.com>
2026-03-09 16:57:03 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/rafaelalves/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.rafaelalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelalves.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
