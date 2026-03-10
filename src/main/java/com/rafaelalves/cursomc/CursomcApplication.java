38cbfd273e848f57db06eb5bb681ca2a406e4d85
Rafael Alves <alves540@hotmail.com>
2026-03-10 15:46:05 -0300

    Estado e Cidade

--- src/main/java/com/rafaelalves/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelalves/cursomc/CursomcApplication.java
@@ -8,13 +8,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafaelalves.cursomc.domain.Categoria;
+import com.rafaelalves.cursomc.domain.Cidade;
+import com.rafaelalves.cursomc.domain.Estado;
 import com.rafaelalves.cursomc.domain.Produto;
 import com.rafaelalves.cursomc.repositories.CategoriaRepository;
+import com.rafaelalves.cursomc.repositories.CidadeRepository;
+import com.rafaelalves.cursomc.repositories.EstadoRepository;
 import com.rafaelalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
@@ -44,6 +55,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+
 	}
 
 }
---
+++ src/main/java/com/rafaelalves/cursomc/domain/Cidade.java
@@ -0,0 +1,68 @@
+package com.rafaelalves.cursomc.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cidade implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name="estado_id")
+	private Estado estado;
+
+	public Cidade() {
+		super();
+	}
+
+	public Cidade(Integer id, String nome, Estado estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public Cidade(Integer id) {
+		super();
+		this.id = id;
+	}
+	
+	
+
+}
