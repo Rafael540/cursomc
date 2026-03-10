fada3534b4a641a3c4177467421647e5e92770de
Rafael Alves <alves540@hotmail.com>
2026-03-10 16:48:32 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/rafaelalves/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.rafaelalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaelalves.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
